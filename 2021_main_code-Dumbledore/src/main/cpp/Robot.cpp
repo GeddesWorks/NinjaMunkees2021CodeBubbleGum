d14028d6e14a386ed449d6d41a5e2e3b887b5932
NinjaMunkeeNoob <46685520+NinjaMunkeeNoob@users.noreply.github.com>
2021-01-26 20:23:41 -0600

    Updated code and fixed it to 2021

---
+++ 2021_main_code-Dumbledore/src/main/cpp/Robot.cpp
@@ -0,0 +1,575 @@
+
+#include "Robot.h"
+#include <iostream>
+#include <frc/smartdashboard/SmartDashboard.h>
+
+void Robot::RobotInit() {
+  m_chooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
+  m_chooser.AddOption(kAutoNameCustom, kAutoNameCustom);
+  frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
+  frc::Color detectedColor = m_colorSensor.GetColor();m_colorMatcher.AddColorMatch(kBlueTarget);
+
+  m_colorMatcher.AddColorMatch(kGreenTarget);
+  m_colorMatcher.AddColorMatch(kRedTarget);
+  m_colorMatcher.AddColorMatch(kYellowTarget);
+
+  // ECODERS
+  shoot1->ConfigSelectedFeedbackSensor(FeedbackDevice::IntegratedSensor, 0, kTimeoutMs);
+  shoot1->ConfigPeakOutputForward(1);
+  shoot1->ConfigPeakOutputReverse(-1);
+  shoot1->Config_kF(kPIDLoopIdx, 0.1097, kTimeoutMs);
+  shoot1->Config_kP(kPIDLoopIdx, 0.01, kTimeoutMs);
+  shoot1->Config_kI(kPIDLoopIdx, 0.0, kTimeoutMs);
+  shoot1->Config_kD(kPIDLoopIdx, 0.0, kTimeoutMs);
+  shoot1->SetSensorPhase(false);
+  shoot1->SetInverted(false);
+  
+  shoot2->ConfigSelectedFeedbackSensor(FeedbackDevice::IntegratedSensor, 0, kTimeoutMs);
+  shoot2->ConfigPeakOutputForward(1);
+  shoot2->ConfigPeakOutputReverse(-1);
+  shoot2->Config_kF(kPIDLoopIdx, 0.1097, kTimeoutMs);
+  shoot2->Config_kP(kPIDLoopIdx, 0.01, kTimeoutMs);
+  shoot2->Config_kI(kPIDLoopIdx, 0.0, kTimeoutMs);
+  shoot2->Config_kD(kPIDLoopIdx, 0.0, kTimeoutMs);
+  shoot2->SetSensorPhase(false);
+  shoot2->SetInverted(false);
+
+  climbPID.SetP(kPe);
+  climbPID.SetI(kI);
+  climbPID.SetD(kD);
+  climbPID.SetIZone(kIz);
+  climbPID.SetFF(kFF);
+  climbPID.SetOutputRange(kMinOutput, kMaxOutput);
+
+  index->ConfigSelectedFeedbackSensor(FeedbackDevice::QuadEncoder, 0, kTimeoutMs);
+
+  T1.Start();
+  T2.Start();
+}
+
+/**
+ * This function is called every robot packet, no matter the mode. Use
+ * this for items like diagnostics that you want ran during disabled,
+ * autonomous, teleoperated and test.
+ *
+ * <p> This runs after the mode specific periodic functions, but before
+ * LiveWindow and SmartDashboard integrated updating.
+ */
+void Robot::RobotPeriodic() {}
+
+/**
+ * This autonomous (along with the chooser code above) shows how to select
+ * between different autonomous modes using the dashboard. The sendable chooser
+ * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
+ * remove all of the chooser code and uncomment the GetString line to get the
+ * auto name from the text box below the Gyro.
+ *
+ * You can add additional auto modes by adding additional comparisons to the
+ * if-else structure below with additional strings. If using the SendableChooser
+ * make sure to add them to the chooser code above as well.
+ */
+void Robot::AutonomousInit() {
+  m_autoSelected = m_chooser.GetSelected();
+  // m_autoSelected = SmartDashboard::GetString("Auto Selector",
+  //     kAutoNameDefault);
+  std::cout << "Auto selected: " << m_autoSelected << std::endl;
+
+  if (m_autoSelected == kAutoNameCustom) {
+    // Custom Auto goes here
+  } else {
+    // Default Auto goes here
+  }
+}
+
+void Robot::AutonomousPeriodic() {
+  if (m_autoSelected == kAutoNameCustom) {
+    // Custom Auto goes here
+  } else {
+    // Default Auto goes here
+    shooterActualSpeed = shoot1->GetSelectedSensorVelocity();
+    shooterTargetSpeed = 4500; //Replace this with real number
+
+    shoot1->Set(ControlMode::Velocity, shooterTargetSpeed); 
+    shoot2->Set(ControlMode::Velocity, shooterTargetSpeed * -1);
+    if(shooterActualSpeed < shooterTargetSpeed + shooterdeadzone && shooterActualSpeed > shooterTargetSpeed - shooterdeadzone){
+      shooterIsRunning = true;
+    }
+    else{
+      shooterIsRunning = false;
+    }
+
+    if (shooterIsRunning == true){
+      index->Set(ControlMode::Velocity, -1); 
+    }
+    else{
+      index->Set(ControlMode::Velocity, 0);
+    }
+    
+  }
+}
+
+void Robot::TeleopInit() {
+ 
+}
+
+void Robot::TeleopPeriodic() {
+
+  ColorPizza();
+  Drive();
+  Shooter();
+  Intake();
+  Climber();
+  Index();
+  LED();
+  //Testing();
+
+  frc::SmartDashboard::PutBoolean("ballIn", ballSwitch.Get());
+  frc::SmartDashboard::PutNumber("frontRightEncoder", frontRightEncoder.GetVelocity());
+  frc::SmartDashboard::PutNumber("frontLeftEncoder", frontLeftEncoder.GetVelocity());
+  frc::SmartDashboard::PutNumber("rearRightEncoder", rearRightEncoder.GetVelocity());
+  frc::SmartDashboard::PutNumber("rearLeftEncoder", rearLeftEncoder.GetVelocity());
+  frc::SmartDashboard::PutNumber("climber", climbEnc.GetPosition());
+
+
+  std::shared_ptr<NetworkTable> table = nt::NetworkTableInstance::GetDefault().GetTable("limelight");
+  double targetArea = table->GetNumber("ta",0.0);
+
+}
+
+  void Robot::ColorPizza() {
+    double confidence = 0.0;
+    frc::Color detectedColor = m_colorSensor.GetColor();
+    std::string colorString;
+    std::string seenColor;
+    frc::Color matchedColor = m_colorMatcher.MatchClosestColor(detectedColor, confidence);
+    
+    
+    if (matchedColor == kBlueTarget) {
+      colorString = "Red";
+      seenColor = "blue";
+    } else if (matchedColor == kRedTarget) {
+      colorString = "Blue";
+      seenColor = "red";
+    } else if (matchedColor == kGreenTarget) {
+      colorString = "Yellow";
+      seenColor = "green";
+    } else if (matchedColor == kYellowTarget) {
+      colorString = "Green";
+      seenColor = "yellow";
+    } else {
+      colorString = "Unknown";
+    }
+
+    
+
+    std::string gameData;
+    gameData = frc::DriverStation::GetInstance().GetGameSpecificMessage();
+    if(gameData.length() > 0){
+      switch (gameData[0]){
+        case 'B' :
+          //Blue case code
+          break;
+        case 'G' :
+          //Green case code
+          break;
+        case 'R' :
+          //Red case code
+          break;
+        case 'Y' :
+          //Yellow case code
+          break;
+        default :
+          //This is corrupt data
+          break;
+      }
+    } 
+    else {
+      //Code for no data received yet
+    }
+    if(buttonBoard.GetRawButton(8)){
+      if(gameData == "G"){
+        // Checks green---------
+        if(colorString == "Green" || haveHitColor == true){ 
+          haveHitColor = true;
+          if(seenColor == "blue"){
+            barDrive->Set(ControlMode::PercentOutput, -.25);
+          }
+          else if(seenColor == "red"){
+            barDrive->Set(ControlMode::PercentOutput, .25);
+          }
+          else if(seenColor == "yellow"){
+            barDrive->Set(ControlMode::PercentOutput, 0);
+          }
+          else{
+            haveHitColor = false;
+          }
+        }
+        
+        else{
+          barDrive->Set(ControlMode::PercentOutput, -.75);
+        }
+      }
+
+      else if (gameData == "R"){
+        // Checks red---------
+        if(colorString == "Red" || haveHitColor == true){ 
+          haveHitColor = true;
+          if(seenColor == "green"){
+            barDrive->Set(ControlMode::PercentOutput, -.25);
+          }
+          else if(seenColor == "yellow"){
+            barDrive->Set(ControlMode::PercentOutput, .25);
+          }
+          else if(seenColor == "blue"){
+            barDrive->Set(ControlMode::PercentOutput, 0);
+          }
+          else{
+            haveHitColor = false;
+          }
+        }
+        else{
+          barDrive->Set(ControlMode::PercentOutput, -.75);
+        }
+      }
+      else if (gameData == "B"){
+        // Checks blue---------
+        if(colorString == "Blue" || haveHitColor == true){ 
+          haveHitColor = true;
+          if(seenColor == "yellow"){
+            barDrive->Set(ControlMode::PercentOutput, -.25);
+          }
+          else if(seenColor == "green"){
+            barDrive->Set(ControlMode::PercentOutput, .25);
+          }
+          else if(seenColor == "red"){
+            barDrive->Set(ControlMode::PercentOutput, 0);
+          }
+          else{
+            haveHitColor = false;
+          }
+        }
+        else{
+          barDrive->Set(ControlMode::PercentOutput, -.75);
+        }
+      }
+      else if (gameData == "Y"){
+        // Checks yellow---------
+        if(colorString == "Yellow" || haveHitColor == true){ 
+          haveHitColor = true;
+          if(seenColor == "red"){
+            barDrive->Set(ControlMode::PercentOutput, -.25);
+          }
+          else if(seenColor == "blue"){
+            barDrive->Set(ControlMode::PercentOutput, .25);
+          }
+          else if(seenColor == "green"){
+            barDrive->Set(ControlMode::PercentOutput, 0);
+          }
+          else{
+            haveHitColor = false;
+          }
+        }
+        else{
+          barDrive->Set(ControlMode::PercentOutput, -.75);
+        }
+      }
+    }
+    else{
+      barDrive->Set(ControlMode::PercentOutput, 0);
+    }
+    frc::SmartDashboard::PutString("Detected color", colorString);
+    frc::SmartDashboard::PutString("Target color", gameData);
+    frc::SmartDashboard::PutNumber("time", T1.Get());
+    /*if(T1.Get() == .15){
+      T1.Reset();
+      
+      if(seenColor == "yellow" && isYellow == false){
+        rotations += 1;
+        isYellow = true;
+      }
+      else if(seenColor != "yellow" && isYellow == true){
+        isYellow == false;
+      }
+    }
+    if(rotations >= 8){
+      frc::SmartDashboard::PutBoolean("rotations", true);
+    }
+    else{
+      frc::SmartDashboard::PutBoolean("rotations", false);
+    }*/
+  }
+
+  void Robot::Intake() {
+    bool up = upSwitch.Get();
+    bool down = downSwitch.Get();
+    if(up == true){
+      frc::SmartDashboard::PutString("Intake is", "UP");
+    }
+    else if(down == true){
+      frc::SmartDashboard::PutString("Intake is", "DOWN");
+    }
+    else{
+      frc::SmartDashboard::PutString("Intake is", "In Between");
+    }
+    
+
+    // upSwitch = 1 when intake is up
+    // downSwitch = 1 when intake is down
+
+    if(buttonBoard.GetRawButton(1) == 1 && up == false){
+      intakeMove->Set(ControlMode::PercentOutput, -.6);
+    }
+    else if(buttonBoard.GetRawButton(5) == 1 && down == false){
+      intakeMove->Set(ControlMode::PercentOutput, .2);
+    }
+    else{
+      intakeMove->Set(ControlMode::PercentOutput, 0);
+    }
+    
+    if(buttonBoard.GetRawButton(9)){  //Has intake button been pushed?
+      if(buttonPressed == false){
+        buttonPressed = true;
+        if(isRun == false){
+          intakeRun->Set(ControlMode::PercentOutput, -.8);
+          isRun = true;
+        }
+        else{
+          isRun = false;
+          intakeRun->Set(ControlMode::PercentOutput, 0);
+        }
+      } 
+      
+    } 
+    else{
+      buttonPressed = false;
+    }
+  }
+
+  void Robot::Drive() {
+    LBd = JLeft.GetY();
+    RBd = JRight.GetY();
+
+    if (buttonBoard.GetRawButton(10) == 0){
+
+      //Left deadZone
+      if (LBd > deadZone){
+        Ld = ((LBd - deadZone) * (1 / (1 - deadZone)));
+      }
+      else if (LBd < - deadZone){
+        Ld = ((LBd + deadZone) * (-1 / (-1 + deadZone)));
+      }
+      else{
+        Ld = 0;
+      }
+
+      //Right deadZone
+      if (RBd > deadZone){
+        Rd = ((RBd - deadZone) * (1 / (1 - deadZone)));
+      }
+      else if (RBd < - deadZone){
+        Rd = ((RBd + deadZone) * (-1 / (-1 + deadZone)));
+      }
+      else{
+        Rd = 0;
+      }
+    }
+
+    
+
+    //Aiming-
+    std::shared_ptr<NetworkTable> table = NetworkTable::GetTable("limelight");
+    float tx = table->GetNumber("tx",0.0);
+    //double targetOffsetAngle_Horizontal = table->GetNumber("tx",0.0);
+    double targetOffsetAngle_Vertical = table->GetNumber("ty",0.0);
+    //double targetArea = table->GetNumber("ta",0.0);
+    //double targetSkew = table->GetNumber("ts",0.0);
+
+    if (buttonBoard.GetRawButton(10)){
+      float heading_error = -tx;
+      float steering_adjust = 0.0f;
+      if (tx > 1.0){
+              steering_adjust = Kp*heading_error - min_command;
+      }
+      else if (tx < 1.0){
+              steering_adjust = Kp*heading_error + min_command;
+      }
+      Ld -= steering_adjust ;
+      Rd += steering_adjust;
+      frc::SmartDashboard::PutNumber("steering", steering_adjust);
+      frc::SmartDashboard::PutNumber("tx", tx);
+    }
+    if(tx < .1 && tx > -.1){
+      aimed = true;
+    }
+    else{
+      aimed = false;
+    }
+    
+    angleOfCameraFromTarget = targetOffsetAngle_Vertical;
+    distanceFromTarget = 500; // (hightOfTarget - hightOfCamera) / tan(angleOfCamera + angleOfCameraFromTarget);
+
+    frc::SmartDashboard::PutNumber("Distance From Target", distanceFromTarget); 
+
+    if(JLeft.GetRawButton(1) == 1){
+      Ld = Ld;
+    }
+    else{
+      Ld = Ld * .5;
+    }
+    if(JRight.GetRawButton(1) == 1){
+      Rd = Rd;
+    }
+    else{
+      Rd = Rd * .5;
+    }
+
+
+    m_left.Set(Ld * -1);
+    m_right.Set(Rd);
+  }
+
+  void Robot::Shooter(){
+
+		double leftYstick = JLeft.GetY();
+
+		shooterActualSpeed = shoot1->GetSelectedSensorVelocity();
+    shooterActualSpeed2 = shoot2->GetSelectedSensorVelocity();
+    frc::SmartDashboard::PutNumber("Shooter Actual Speed", shooterActualSpeed);
+    //frc::SmartDashboard::PutNumber("Shooter Actual Speed2", shooterActualSpeed);
+
+    shooterTargetSpeed = 4000; //distanceFromTarget; // * some number;
+
+    if(buttonBoard.GetRawButton(11)){
+      shoot1->Set(ControlMode::Velocity, shooterTargetSpeed); 
+      shoot2->Set(ControlMode::Velocity, shooterTargetSpeed * -1);
+      ballUp->Set(ControlMode::PercentOutput, 1); 
+      if(shooterActualSpeed < (shooterTargetSpeed + shooterdeadzone) && shooterActualSpeed > (shooterTargetSpeed - shooterdeadzone)){
+        shooterIsRunning = true;
+      }
+      else{
+        shooterIsRunning = false;
+      }
+    }
+    else{
+
+      shoot1->Set(ControlMode::Velocity, 0); 
+      shoot2->Set(ControlMode::Velocity, 0); 
+      ballUp->Set(ControlMode::PercentOutput, 0);
+      shooterIsRunning = false;
+    }
+    frc::SmartDashboard::PutNumber("Shooter Target Speed", shooterTargetSpeed);
+    if(shooterIsRunning == true){
+      frc::SmartDashboard::PutBoolean("Shooter Is Shooting", true);
+    }
+    else{
+      frc::SmartDashboard::PutBoolean("Shooter Is Shooting", false);
+    }
+  }
+
+  void Robot::Climber(){
+    bool goUp = buttonBoard.GetRawButton(2);
+    bool goDown = buttonBoard.GetRawButton(6);
+
+    if(goUp == 1){
+      pos = posUp;
+    }
+    else if(goDown == 1){
+     
+      pos = posDown;
+    }
+    else{}
+
+    if(buttonBoard.GetRawButton(3)){
+      barDrive->Set(ControlMode::PercentOutput, -1);
+    }
+    else if(buttonBoard.GetRawButton(7)){
+      barDrive->Set(ControlMode::PercentOutput, 1);
+    }
+    else{
+      barDrive->Set(ControlMode::PercentOutput, 0);
+    }
+
+    //climbPID.SetReference(pos, rev::ControlType::kPosition);
+    
+    climber.Set(testJ.GetY());
+  }
+
+  void Robot::Index(){
+    if (buttonBoard.GetRawButton(11) && shooterIsRunning == true){
+      index->Set(ControlMode::PercentOutput, -.5); 
+      //ballUp->Set(ControlMode::PercentOutput, 1);
+    }
+    else if(buttonBoard.GetRawButton(4) && indexShift == false){
+      T2.Reset();
+      indexShift = true;
+      
+    }
+    else if(indexShift == false && buttonBoard.GetRawButton(11) == false){
+      index->Set(ControlMode::PercentOutput, 0); // 0
+      
+    }
+    else{
+      //ballUp->Set(ControlMode::PercentOutput, 0); 
+    }
+
+    if (indexShift == true && T2.Get() <= .57){
+      index->Set(ControlMode::PercentOutput, -.5); 
+    }
+    else{
+      indexShift = false;
+    }
+
+    
+  }
+
+  void Robot::LED(){
+    
+    if(aimed == true){
+      LEDcontrol.Set(-0.91);
+      frc::SmartDashboard::PutBoolean("Aimed", true);
+    }
+    else{
+      LEDcontrol.Set(-0.79);
+      frc::SmartDashboard::PutBoolean("Aimed", false);
+    }
+
+  }
+
+  void Robot::Testing(){
+    speed = frc::SmartDashboard::GetNumber("DB/Slider 0", 0);
+    Ospeed = speed / 100;
+    
+
+    if(buttonBoard.GetRawButton(11)){
+        shoot1->Set(ControlMode::PercentOutput, Ospeed);
+        shoot2->Set(ControlMode::PercentOutput, Ospeed * -1);
+        ballUp->Set(ControlMode::PercentOutput, 1);
+        if(shooterActualSpeed < shooterTargetSpeed + shooterdeadzone && shooterActualSpeed > shooterTargetSpeed - shooterdeadzone){
+          shooterIsRunning = true;
+        }
+        else{
+          shooterIsRunning = false;
+        }
+      }
+      else{
+        shoot1->Set(ControlMode::Velocity, 0); 
+        shoot2->Set(ControlMode::Velocity, 0); 
+        shooterIsRunning = false;
+        ballUp->Set(ControlMode::PercentOutput, 0);
+      }
+      frc::SmartDashboard::PutNumber("Shooter Target Speed", shooterTargetSpeed);
+      if(shooterIsRunning == true){
+        frc::SmartDashboard::PutBoolean("Shooter Is Shooting", true);
+      }
+      else{
+        frc::SmartDashboard::PutBoolean("Shooter Is Shooting", false);
+      }
+  }
+
+void Robot::TestPeriodic() {
+  
+}
+
+#ifndef RUNNING_FRC_TESTS
+int main() { return frc::StartRobot<Robot>(); }
+#endif
---
+++ 2021_main_code-Dumbledore/src/main/include/Robot.h
@@ -0,0 +1,192 @@
+#pragma once
+
+#include "frc/WPILib.h"
+#include <string>
+#include <frc/smartdashboard/SendableChooser.h>
+#include <frc/smartdashboard/SmartDashboard.h>
+
+#include <frc/spark.h>
+#include <cameraserver/CameraServer.h>
+
+#include <frc/DigitalInput.h>
+#include <wpi/raw_ostream.h>
+#include <frc/encoder.h>
+#include <frc/TimedRobot.h>
+#include <frc/Joystick.h>
+
+#include <frc/util/color.h>
+#include "rev/ColorSensorV3.h"
+#include "rev/ColorMatch.h"
+
+#include <frc/SpeedControllerGroup.h>
+
+#include "networktables/NetworkTable.h"
+#include "networktables/NetworkTableInstance.h"
+
+#include <frc/Talon.h>
+#include "rev/CANSparkMax.h"
+#include "ctre/Phoenix.h"
+#include <frc/timer.h>
+
+
+class Robot : public frc::TimedRobot {
+ public:
+  void RobotInit() override;
+  void RobotPeriodic() override;
+  void AutonomousInit() override;
+  void AutonomousPeriodic() override;
+  void TeleopInit() override;
+  void TeleopPeriodic() override;
+  void TestPeriodic() override;
+// Robot scripts setup
+
+  void Drive();
+  void Shooter();
+  void Intake();
+  void Climber();
+  void ColorPizza();
+  void Index();
+  void LED();
+  void Testing();
+
+  // Input
+
+  frc::Joystick JLeft{0};
+  frc::Joystick JRight{1};
+  frc::Joystick buttonBoard{2};
+  frc::Joystick testJ{3};
+  frc::DigitalInput upSwitch{1};
+  frc::DigitalInput downSwitch{2};
+  frc::DigitalInput ballSwitch{4};
+  
+  static constexpr auto i2cPort = frc::I2C::Port::kOnboard;
+  rev::ColorSensorV3 m_colorSensor{i2cPort};
+  rev::ColorMatch m_colorMatcher;
+ 
+  static constexpr frc::Color kBlueTarget = frc::Color(0.143, 0.427, 0.429);
+  static constexpr frc::Color kGreenTarget = frc::Color(0.197, 0.561, 0.240);
+  static constexpr frc::Color kRedTarget = frc::Color(0.561, 0.232, 0.114);
+  static constexpr frc::Color kYellowTarget = frc::Color(0.361, 0.524, 0.113);
+  bool haveHitColor = false;
+  frc::Timer T1;
+  frc::Timer T2;
+  int rotations = 0;
+  bool rot;
+  bool isYellow = false;
+// drive train setup
+
+  float deadZone = .25;
+ 
+  rev::CANSparkMax frontLeftMotor1{1, rev::CANSparkMax::MotorType::kBrushless};
+  rev::CANSparkMax frontLeftMotor2{2, rev::CANSparkMax::MotorType::kBrushless};
+  rev::CANSparkMax frontRightMotor1{3, rev::CANSparkMax::MotorType::kBrushless};
+  rev::CANSparkMax frontRightMotor2{4, rev::CANSparkMax::MotorType::kBrushless};
+  rev::CANSparkMax rearLeftMotor1{5, rev::CANSparkMax::MotorType::kBrushless};
+  rev::CANSparkMax rearLeftMotor2{6, rev::CANSparkMax::MotorType::kBrushless};
+  rev::CANSparkMax rearRightMotor1{7, rev::CANSparkMax::MotorType::kBrushless};
+  rev::CANSparkMax rearRightMotor2{8, rev::CANSparkMax::MotorType::kBrushless};
+
+
+  rev::CANEncoder frontLeftEncoder = frontLeftMotor1.GetEncoder();
+  rev::CANEncoder frontRightEncoder = frontRightMotor1.GetEncoder();
+  rev::CANEncoder rearLeftEncoder = rearLeftMotor1.GetEncoder();
+  rev::CANEncoder rearRightEncoder = rearRightMotor1.GetEncoder();
+
+  frc::SpeedControllerGroup m_left{frontLeftMotor1, frontLeftMotor2, rearLeftMotor1, rearLeftMotor2};
+  frc::SpeedControllerGroup m_right{frontRightMotor1, frontRightMotor2, rearRightMotor1, rearRightMotor2};
+  
+
+// Aiming------------------
+  std::shared_ptr<NetworkTable> table = nt::NetworkTableInstance::GetDefault().GetTable("limelight");
+ 
+  float Kp = -.1f;
+  float min_command = 0.05f;
+  bool aimed;
+  float distanceFromTarget;
+  float hightOfTarget = 7.5623;
+  float hightOfCamera = 0.8645833;
+  float angleOfCamera = 122;
+  float angleOfCameraFromTarget;
+  
+ // Talon encoder shooter-----------
+  TalonFX * shoot1 = new TalonFX(9);
+  TalonFX * shoot2 = new TalonFX(12);
+  double speed;
+  double Ospeed;
+  const float shooterdeadzone = 300;
+
+  double shooterTargetSpeed;
+  double shooterActualSpeed;
+  double shooterActualSpeed2;
+
+  const float WRIST_kP = 0.001;
+  const float WRIST_kI = 0;
+  const float WRIST_kD = 0;
+  const float WRIST_kF = 0;
+
+  const int kPIDLoopIdx = 0;
+
+  const int kTimeoutMs = 30;
+
+  const float  wrist_low = 0;
+  const float  wrist_pickup = 0;
+  const float  wrist_mid = 4096 / 4;
+  const float  wrist_high = 0.15 * 4096;
+
+  const int kSlotIdx = 0;
+
+// Intake-------------------
+  VictorSPX * intakeMove = new VictorSPX(10);
+  TalonSRX * intakeRun = new TalonSRX(13);
+  TalonSRX * ballUp = new TalonSRX(18);
+  bool buttonPressed = false; //Toggles intake on(true) & off(false)
+  bool isRun = false;
+  
+// Climber------------------
+  rev::CANSparkMax climber{14, rev::CANSparkMax::MotorType::kBrushless};
+  rev::CANEncoder climbEnc = climber.GetEncoder();
+  rev::CANPIDController climbPID = climber.GetPIDController();
+  TalonSRX * barDrive = new TalonSRX(15);  
+
+  double pos;
+  double posUp = 0;
+  double posDown = 0;
+
+  double kPe = 0.2, 
+    kI = 0, 
+    kD = 1, 
+    kIz = 0, 
+    kFF = 0, 
+    kMaxOutput = 1, 
+    kMinOutput = -1;
+
+// Index-------------------
+  TalonSRX * index = new TalonSRX(11);
+  double indexDirection = 0;
+  bool intakeIsRunning;
+  bool shooterIsRunning;
+  bool indexShift = false;
+  bool button2Pressed = false;
+  bool is2Run = false;
+  bool IndexRun = false;
+
+// LED Set-up
+  frc::Spark LEDcontrol{0};
+  bool canShoot;
+
+
+ private:
+  frc::SendableChooser<std::string> m_chooser;
+  const std::string kAutoNameDefault = "Default";
+  const std::string kAutoNameCustom = "My Auto";
+  std::string m_autoSelected;
+  
+  float LBd;
+  float RBd;
+  float Ld;
+  float Rd;
+
+ 
+
+  
+};
